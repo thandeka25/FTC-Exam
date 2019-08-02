31659732e86c5960a1742c4167648119bf911042
Thandeka <ttrchehore@gmail.com>
2019-08-02 22:07:12 +0200

    add functions to contract

---
+++ contracts/shoecontract.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.2;
+
+contract CoShoe {
+  struct Shoe {
+    address owner;
+    string name;
+    string image;
+    bool sold;
+  }
+
+  uint price = 500000000000000000;
+  uint shoesSold = 0;
+
+  Shoe [] public shoes;
+  
+  constructor {
+    shoes.push(Shoe(msg.sender, "", "", false));
+  }
+  
+  function buyShoe payable external (string _name, string _image) {
+    require (shoes.length >= 1, "No shoes left.");
+    require (price == msg.value, "Value is not equal to the price.");
+    shoes.push(Shoe(msg.sender, _name, _image, true)) - 1;
+    shoesSold++;
+  }
+
+  function checkPurchases external view returns(bool [] memory) {
+    bool [] memory checkPur;
+    for (uint i < shoe.length; i=0; i++ ) {
+      if (shoes[i].owner == msg.sender) {
+        checkPur[i] = true;
+      }
+    }
+
+  }
+}
