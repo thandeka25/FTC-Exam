a5929297796ec1495883acce27387203a78b77a3
Thandeka <ttrchehore@gmail.com>
2019-08-04 00:00:42 +0200

    Fix test file for coshoe contract

--- Question 2.1/test/testCoShoe.js
+++ Question 2.1/test/testCoShoe.js
@@ -1,69 +1,60 @@
-const CoShoe = artifacts.require('CoShoe')
+const CoShoe = artifacts.require('CoShoe.sol')
 
 contract('CoShoe', function (accounts) {
   // predefine parameters
-  const owner = 'Cool Song'
-  const name = 'example.com'
-  const image = 'example.com'
-  const sold = 'true'
+  const Footowner = accounts[0];
+  const random_address = accounts[1];
+  const eg_image = "theimage.com";
+  
+  const validateShoe = {
+    name: '',
+    image: '',
+    sold: false,
+    price: 1
+  }
+
   //const songPrice = web3.utils.toWei('1', 'ether')
 
+  //start off without any shoes
+  let num_shoes = 0;
+  let prize = 1;
+
+  /** 
   it('should contain zero shoes in the beginning', async function () {
     // fetch instance of SongRegistry contract
-    let CoShoeInstance = await CoShoe.deployed()
+    //let CoShoeInstance = await CoShoe.deployed()
     // get the number of songs
-    let shoeCounter = await CoShoeInstance.getNumberOfSongs()
+    //let shoeCounter = await CoShoeInstance.getNumberOfSongs()
     // check that there are no songs initially
-    assert.equal(shoeCounter, 0, 'initial number not equal to zero')
-  })
+    //assert.equal(shoeCounter, 0, 'initial number not equal to zero')
+  }) */
 
-  it('should add a shoe to the registry', async function () {
+  context ("Buy Another Shoe", function () {
+  it('transfers ownership correctly', async function () => {
     let CoShoeInstance = await CoShoe.deployed()
     // register a song from account 0
-    await CoShoeInstance.registerSong(
-      owner,
-      name,
-      image,
-      sold,
-    )
-    // get the number of songs
-    let songCounter = await SongRegistryInstance.getNumberOfSongs()
-    // check that there is one song available now
-    assert.equal(songCounter, 1, 'song was not successfully registered')
-    // retrieve the song details
-    let song = await SongRegistryInstance.songs(0)
-    // check that they match the original song details
-    assert.equal(song['owner'], accounts[0], 'owner does not match')
-    assert.equal(song['title'], songTitle, 'title does not match')
-    assert.equal(song['url'], songUrl, 'url does not match')
-    assert.equal(song['price'], songPrice, 'price does not match')
+    await CoShoeInstance.buyShoe(validShoe.name, validShoe.image, {
+      from: shoeOwner
+    })
+    // retrieve the shoes details
+    let shoe = await CoShoeInstance.shoes()
+  
+    // check that they match the original shoe details
+    assert.equal(shoe['owner'], shoeOwner, 'owner does not match')
+    assert.equal(shoe['name'], validShoe.name, 'name does not match')
+    assert.equal(shoe['image'], validShoe.image, 'image url does not match')
+    assert.equal(shoe.sold, false, 'no change in status')
+    assert.equal(validShoe.price, price, "Insufficient funds")
+    num_shoes += 1;
+    console.log(num_shoes)
   })
 
-  it('should only return true for buyers', async function () {
-    let SongRegistryInstance = await SongRegistry.deployed()
-    // call the checkBuyer function from account 0
-    let checkBuyer = await SongRegistryInstance.checkBuyer(0, { from: accounts[0] })
-    // check that it returns true
-    assert.equal(checkBuyer, true, 'Owner is not buyer')
-    // call the checkBuyer function from account 1
-    checkBuyer = await SongRegistryInstance.checkBuyer(0, { from: accounts[1] })
-    // check that it returns false
-    assert.equal(checkBuyer, false, 'Account 1 should not be a buyer')
+  it('should return the correct number of trues', async () => {
+    let CoShoeInstance = await CoShoe.deployed()
+    // call the checkPur function 
+    let checkPurchase = await CoShoeInstance.checkPur()
+ 
   })
 
-  it('should allow account 1 to buy the song', async function () {
-    let SongRegistryInstance = await SongRegistry.deployed()
-    // get the initial balance of account 0
-    let account0InitialBalance = await web3.eth.getBalance(accounts[0])
-    // buy the song from account 1
-    await SongRegistryInstance.buySong(0, { from: accounts[1], value: songPrice })
-    // call checkBuyer from account 1
-    let checkBuyer = await SongRegistryInstance.checkBuyer(0, { from: accounts[1] })
-    // check that it returns true
-    assert.equal(checkBuyer, true, 'Account 1 should be a buyer')
-    // get the new balance of account 0
-    let account0NewBalance = await web3.eth.getBalance(accounts[0])
-    // check that it has increased by the song price
-    assert.equal(account0NewBalance, Number(account0InitialBalance) + Number(songPrice), 'Account 0 was paid by account 1')
-  })
+
 })
