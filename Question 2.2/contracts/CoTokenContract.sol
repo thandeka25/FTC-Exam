8c3d2856bef4e3a63dd38428b074764443f2fa64
Thandeka <ttrchehore@gmail.com>
2019-08-03 19:25:04 +0200

    Contract edits

--- Question 2.2/contracts/CoTokenContract.sol
+++ Question 2.2/contracts/CoTokenContract.sol
@@ -1,5 +1,31 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.5.0;
 
-contract CoToken {
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+
+contract CoToken is Ownable, ERC20 {
+    uint256 constant public decimals = 10**18;
+    uint public totsupply;
+
+    function buyPrice (uint _tokensupply, uint num) public returns(uint) {
+        uint tokenprice = ((_tokensupply /100) + (2/10)) * decimals;
+        //tokensupply.add(num);
+        return tokenprice;
+    }
+
+    function mint (uint256 amount) public {
+        _mint(msg.sender, amount);
+        totsupply = totsupply + amount;
+    }
+
+    function sellPrice (uint _tokensupply) public returns (uint) {
+        uint tokenprice = ((_tokensupply /100) + (2/10)) * decimals;
+    }
     
+    function burn(uint _numtokens, uint _amount) payable public {
+        require(msg.value >= buyprice (_amount));
+    }
+
+
 }
