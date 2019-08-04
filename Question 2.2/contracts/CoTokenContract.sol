3b56128b1493eeeca5010d1e78d4b644130f5270
Thandeka <ttrchehore@gmail.com>
2019-08-04 09:46:06 +0200

    Add integral function to contract

--- Question 2.2/contracts/CoTokenContract.sol
+++ Question 2.2/contracts/CoTokenContract.sol
@@ -6,8 +6,9 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/ownership/Ownable.sol";
 
 
+
 contract CoToken is Ownable, ERC20 {
-    uint public totsupply;
+    uint public totsupply;  //current number of tokens available
     uint public tokenBuyPrice_;
     uint public tokenSellPrice_;
 
@@ -16,6 +17,25 @@ contract CoToken is Ownable, ERC20 {
         //tokensupply.add(num);
         return tokenBuyPrice_;
     }
+
+    /** 
+    function _integralFunction (uint _quantity) internal returns (uint){
+        uint reqBuyPrice = ((_quantity * 10**16)**2)  + (_quantity*2*10**17) //the integral function to calcbuying price required to buy a certain amount of 
+        return reqBuyPrice
+    } 
+    */
+
+    /** 
+    function integralBuyPrice (uint _amount) public returns(uint) {
+        uint Q0 = totsupply; //token supply before minting
+        uint Q1 = totsupply + _amount; //token supply after potential buy
+        uint lower_bound = reqBuyPrice(Q0);
+        uint upper_bound = reqBuyPrice(Q1);
+        tokenBuyPrice_ = upper_bound - lower_bound;
+        return tokenBuyPrice_
+    }
+    */
+
 
     function mint (uint256 amount) public payable {
         require(msg.value >= buyPrice()*amount);
@@ -27,6 +47,17 @@ contract CoToken is Ownable, ERC20 {
         tokenSellPrice_ = (totsupply * 10**16)  + (2*10**17);
         return tokenSellPrice_;
     }
+
+    /**
+    function integralSellPrice (uint _amount) public returns(uint) {
+        uint Q0 = totsupply; //token supply before selling back to curve
+        uint Q1 = totsupply - _amount; //token supply after potential sell
+        uint lower_bound = reqBuyPrice(Q0);
+        uint upper_bound = reqBuyPrice(Q1);
+        tokenSellPrice_ = upper_bound - lower_bound;
+        return tokenSellPrice_
+    }
+     */
     
     function burn(uint _n) payable public {
         require(msg.value >= sellPrice ()*_n);
