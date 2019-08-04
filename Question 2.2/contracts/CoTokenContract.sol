5293ef11a2725f8a8f8b19c650345f2b6e080cc0
Thandeka <ttrchehore@gmail.com>
2019-08-04 22:10:57 +0200

    Edit contracts

--- Question 2.2/contracts/CoTokenContract.sol
+++ Question 2.2/contracts/CoTokenContract.sol
@@ -11,29 +11,26 @@ contract CoToken is Ownable, ERC20 {
     uint public tokenBuyPrice_;
     uint public tokenSellPrice_;
 
+    /**
     function buyPrice () public view returns(uint) {
         tokenBuyPrice_ = (totsupply * 10**16) + (2*10**17);
         //tokensupply.add(num);
         return tokenBuyPrice_;
-    }
+    } */
 
-    /**
     function _integralFunction (uint _quantity) internal returns (uint){
-        uint reqBuyPrice = ((_quantity * 10**16)**2)  + (_quantity*2*10**17) //the integral function to calcbuying price required to buy a certain amount of
-        return reqBuyPrice
+        uint reqBuyPrice = ((_quantity * 10**16)**2) + (_quantity*2*10**17); //the integral function
+        return reqBuyPrice;
     }
-    */
 
-    /**
     function integralBuyPrice (uint _amount) public returns(uint) {
         uint Q0 = totsupply; //token supply before minting
         uint Q1 = totsupply + _amount; //token supply after potential buy
         uint lower_bound = reqBuyPrice(Q0);
         uint upper_bound = reqBuyPrice(Q1);
         tokenBuyPrice_ = upper_bound - lower_bound;
-        return tokenBuyPrice_
+        return tokenBuyPrice_;
     }
-    */
 
     function mint (uint256 amount) public payable {
         require(msg.value >= buyPrice()*amount, "Not enough funds");
@@ -41,21 +38,21 @@ contract CoToken is Ownable, ERC20 {
         totsupply = totsupply + amount;
     }
 
+    /**
     function sellPrice () public view returns (uint) {
         tokenSellPrice_ = (totsupply * 10**16) + (2*10**17);
         return tokenSellPrice_;
-    }
+    } */
 
-    /**
     function integralSellPrice (uint _amount) public returns(uint) {
         uint Q0 = totsupply; //token supply before selling back to curve
         uint Q1 = totsupply - _amount; //token supply after potential sell
         uint lower_bound = reqBuyPrice(Q0);
         uint upper_bound = reqBuyPrice(Q1);
         tokenSellPrice_ = upper_bound - lower_bound;
-        return tokenSellPrice_
+        return tokenSellPrice_;
     }
-     */
+
     function burn(uint _n) public payable {
         require(msg.value >= sellPrice ()*_n, "Funds not enugh to buy tokens");
         //mint(_n);
