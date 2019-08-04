8a394812ae48bfdd25dfd50d59949bb481c018e3
Thandeka <ttrchehore@gmail.com>
2019-08-05 01:59:53 +0200

    configure truffle js

--- Question 2.2/truffle-config.js
+++ Question 2.2/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "fj4jll3k.....";
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+//const infuraKey = "fj4jll3k.....";
 //
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const fs = require('fs');
+//const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -60,13 +60,13 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
-       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-       network_id: 3,       // Ropsten's id
-       gas: 5500000,        // Ropsten has a lower block limit than mainnet
-       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    //ropsten: {
+       //provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+       //network_id: 3,       // Ropsten's id
+       //gas: 5500000,        // Ropsten has a lower block limit than mainnet
+       //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+       //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
 
     // Useful for private networks
@@ -75,16 +75,16 @@ module.exports = {
       // network_id: 2111,   // This network is yours, in the cloud.
       // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+  }
 
   // Set default mocha options here, use special reporters etc.
-  mocha: {
+  //mocha: {
     // timeout: 100000
-  },
+  //},
 
   // Configure your compilers
-  compilers: {
-    solc: {
+  //compilers: {
+    //solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
@@ -94,6 +94,6 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
-  }
+    //}
+  //}
 }
